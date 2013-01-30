9f3a366d24ae56094ed1f67e9f463a347dea9ecb
saumya <saumya>
2013-01-30 13:23:29 +0530

    added JQueryRequireJS single lib

--- src/main.js
+++ src/main.js
@@ -1,3 +1,13 @@
+/*
 (function ($) {
 	console.log('init app');
 } (jQuery));
+*/
+//RequireJS
+require(["jquery"], function($) {
+    //the jquery.alpha.js and jquery.beta.js plugins have been loaded.
+    $(function() {
+    	console.log('Application initialised ! ');
+        //console.log($('body'));
+    });
+});
