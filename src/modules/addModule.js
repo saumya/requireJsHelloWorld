1ab9ed99c15a2a46f71e07e4b2c677545ad01bfb
saumya <saumya>
2013-01-30 15:43:29 +0530

    done with first module

--- src/modules/addModule.js
+++ src/modules/addModule.js
@@ -1,3 +1,9 @@
 define('modules/addModule',function(){
 	return 'Add Module : Hello World.';
 });
+/*
+//just a HACK : should work !! but not working !!!
+define('modules/addHackModule',function(){
+	return 'Add HACK Module : Hello World.';
+});
+*/
